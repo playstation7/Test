b3d5d3ab5fed038eb9d9c94706163c8fb70bd979
playstation7 <neket.1992@mail.ru>
2021-08-06 17:18:12 +0300

    Fisrt commit

---
+++ Assets/Scripts/SliderScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour
+{
+    public GameObject menuPanel;
+    public Slider slider;
+    public bool gameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(progressBar());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private IEnumerator progressBar() 
+    {
+        yield return new WaitForSeconds(0.10f);
+        slider.value -= 0.3f;
+        if (slider.value == 0) 
+        {
+            menuPanel.SetActive(true);
+            gameOver = true;
+        }
+        StartCoroutine(progressBar());
+    }
+
+    public void increaseProgressBar(int points) 
+    {
+        slider.value += (float)points;
+    }
+}
